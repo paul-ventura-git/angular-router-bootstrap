f1731cb293b732795f93fd75bd5a779f302c8b66
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-13 21:18:56 -0500

    Folder structure and basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'products', component: ProductsComponent },
+  { path: 'customers', component: CustomersComponent }
+];
