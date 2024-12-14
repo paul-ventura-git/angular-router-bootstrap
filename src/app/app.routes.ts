861c3f459153ec3ada46f11eab35784becf6c55b
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-13 22:34:32 -0500

    No Found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { CustomersComponent } from './customers/customers.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent},
+  { path: '', component: HomeComponent},
   { path: 'products', component: ProductsComponent },
   { path: 'products/:category/:id', component: ProductDetailComponent },
-  { path: 'customers', component: CustomersComponent }
+  { path: 'customers', component: CustomersComponent },
+  { path: '**', component: NotFoundComponent }
 ];
