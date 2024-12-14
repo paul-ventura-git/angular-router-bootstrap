9b2a50e3adb2be3a8ca89859490b0232e842e75f
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-13 21:27:39 -0500

    Product Details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { CustomersComponent } from './customers/customers.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'products', component: ProductsComponent },
+  { path: 'products/:category/:id', component: ProductDetailComponent },
   { path: 'customers', component: CustomersComponent }
 ];
